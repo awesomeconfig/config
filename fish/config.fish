c0aec0ec042094fcdedf54713b95c7cd6cd62fdc
awesomeconfig <122733261+awesomeconfig@users.noreply.github.com>
2023-01-15 14:53:15 +0100

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+# ~/.config/fish/config.fish
+
+starship init fish | source
+
+set -U fish_greeting
+neofetch
+
+    # Commands to run in interactive sessions can go here
+end
